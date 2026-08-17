5730baffd69c5deb25e99b079dd7dd2707f8fa07
sanjaynep <poudelsanjay93@gmail.com>
2026-08-17 09:35:58 +0000

    added the authorization

--- backend/mero_wiki/mero_wiki/settings.py
+++ backend/mero_wiki/mero_wiki/settings.py
@@ -154,8 +154,8 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=20),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
